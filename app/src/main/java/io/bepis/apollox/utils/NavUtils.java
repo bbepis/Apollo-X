6c0bd7aee0f259f9954ed2330d872da39b7fb9d4
Bepis <36346617+bbepis@users.noreply.github.com>
2020-11-27 17:42:00 +1100

    Update manifest name and fix visual bug when switching to player activity

--- app/src/main/java/io/bepis/apollox/utils/NavUtils.java
+++ app/src/main/java/io/bepis/apollox/utils/NavUtils.java
@@ -113,11 +113,11 @@ public final class NavUtils {
      */
     public static void openAudioPlayer(final Activity activity) {
         final Intent intent = new Intent(activity, AudioPlayerActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
         activity.startActivity(intent);
-        activity.finish();
     }
 
     /**
@@ -143,8 +143,8 @@ public final class NavUtils {
         final Intent intent = new Intent(activity, HomeActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(intent);
-        activity.finish();
+//        activity.finish();
     }
 }
